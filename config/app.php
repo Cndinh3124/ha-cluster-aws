24d0a9dc77e07912e3abc91cff4559bb97cf2b3d
Cong Dinh Nguyen <155534726+Cndinh3124@users.noreply.github.com>
2026-05-25 18:35:29 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 const APP_NAME = 'Hội Sinh Viên TDC';
 
-const APP_URL = 'http://localhost';
+define('APP_URL', getenv('APP_URL') ?: '');
 
 const UPLOAD_POST_DIR = __DIR__ . '/../uploads/posts/';
 const UPLOAD_SLIDE_DIR = __DIR__ . '/../uploads/slides/';
